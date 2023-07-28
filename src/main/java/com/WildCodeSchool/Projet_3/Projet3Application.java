e5a904ce1d66515852bc7c421d94deccb0490552
tophe-w <128515611+tophe-w@users.noreply.github.com>
2023-07-28 11:06:39 +0200

    Creation Repo Spring

---
+++ src/main/java/com/WildCodeSchool/Projet_3/Projet3Application.java
@@ -0,0 +1,13 @@
+package com.WildCodeSchool.Projet_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/WildCodeSchool/Projet_3/Projet3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WildCodeSchool.Projet_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
