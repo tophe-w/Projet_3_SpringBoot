00c69fd6cb1081d3ad147bb4a690145d61241dc0
tophe-w <128515611+tophe-w@users.noreply.github.com>
2023-08-14 10:11:22 +0200

    Mise ne page

--- src/main/java/com/WildCodeSchool/Projet_3/entity/Message.java
+++ src/main/java/com/WildCodeSchool/Projet_3/entity/Message.java
@@ -18,9 +18,9 @@ public class Message {
     private User user;
 
     public User getUser() {
-    return user;
+    return user;        
 }
-public void setUser(User user) {
+    public void setUser(User user) {
     this.user = user;
 }
     public int getId() {
