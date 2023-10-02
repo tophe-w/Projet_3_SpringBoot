ffcd6ca36491c3d8f719e87888f35014264a1e68
Bensadia Omar <bensadia.omar@gmail.com>
2023-10-02 10:56:23 +0200

    dotenv en moins

--- src/main/java/com/WildCodeSchool/Projet_3/Projet3Application.java
+++ src/main/java/com/WildCodeSchool/Projet_3/Projet3Application.java
@@ -3,7 +3,7 @@ package com.WildCodeSchool.Projet_3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
+
 
 
 
@@ -12,9 +12,6 @@ public class Projet3Application {
 
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		String secretKey = dotenv.get("SECRET_KEY");
-		System.out.println("SECRET_KEY: " + secretKey);
 		SpringApplication.run(Projet3Application.class, args);
 	}
 
