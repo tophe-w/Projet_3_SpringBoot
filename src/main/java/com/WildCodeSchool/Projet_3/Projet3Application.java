9af2b37596d864fdd3e51ed99c8c9e6f2e6f207e
Bensadia Omar <bensadia.omar@gmail.com>
2023-10-04 11:42:57 +0200

    mise en place du .env

--- src/main/java/com/WildCodeSchool/Projet_3/Projet3Application.java
+++ src/main/java/com/WildCodeSchool/Projet_3/Projet3Application.java
@@ -1,9 +1,10 @@
 package com.WildCodeSchool.Projet_3;
-
-
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+;
 
 
 
@@ -11,13 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Projet3Application {
 
-	
 
+@Autowired
+    
 
 	public static void main(String[] args) {
-		
-		
 		SpringApplication.run(Projet3Application.class, args);
 	}
+
+	
 
 }
